04f2e8e2d3f131f856b59ddc4f6587d8eca65a41
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2022-08-21 18:15:08 +0530

    removed ascii

--- main.py
+++ main.py
@@ -11,15 +11,6 @@ def getpage():
     page_request = input('what do you want to search? : ').replace(" ", "_")
     return page_request
 
-def asciiart():
-    print("""  
-     __     __     __     __  __     __     ______     ______     ______     ______     ______     __  __    
-    /\ \  _ \ \   /\ \   /\ \/ /    /\ \   /\  ___\   /\  ___\   /\  __ \   /\  == \   /\  ___\   /\ \_\ \   
-    \ \ \/ ".\ \  \ \ \  \ \  _"-.  \ \ \  \ \___  \  \ \  __\   \ \  __ \  \ \  __<   \ \ \____  \ \  __ \  
-     \ \__/".~\_\  \ \_\  \ \_\ \_\  \ \_\  \/\_____\  \ \_____\  \ \_\ \_\  \ \_\ \_\  \ \_____\  \ \_\ \_\ 
-      \/_/   \/_/   \/_/   \/_/\/_/   \/_/   \/_____/   \/_____/   \/_/\/_/   \/_/ /_/   \/_____/   \/_/\/_/                                                                                                        
-    """)
-
 def extractpage(content):
     page = requests.get(f"https://en.wikipedia.org/wiki/{content}").text # get the webpage from wikipedia
     soup = BeautifulSoup(str(page), 'lxml') # input the content to beautiful soup
@@ -29,7 +20,6 @@ def extractpage(content):
     paras = soup.find_all('p') #find all paragraphs
     return title, sections, paras
 
-asciiart()
 html_content = getpage()
 title, sections, paras = extractpage(html_content)
 print(title.text)
